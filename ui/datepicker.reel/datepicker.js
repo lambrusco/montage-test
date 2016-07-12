b9b5c72a69a6cfac5d471cece0d3d13ab24dad5f
Pier Paolo Orioli <noreply>
2016-07-12 09:31:15 +0000

    Add component datepicker

---
+++ ui/datepicker.reel/datepicker.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/datepicker.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Datepicker
+ * @extends Component
+ */
+exports.Datepicker = Component.specialize(/** @lends Datepicker# */ {
+    constructor: {
+        value: function Datepicker() {
+            this.super();
+        }
+    }
+});
